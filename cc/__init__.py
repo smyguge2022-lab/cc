a245f6d1ad8a4f5150fc2aa75c3ba5254868a263
smyguge2022-lab <smyguge2022@gmail.com>
2025-12-31 17:52:19 +0800

    Add feature extraction for packets (#4)

--- cc/__init__.py
+++ cc/__init__.py
@@ -1,3 +1,22 @@
+"""Core feature extraction and packet structures."""
+
+from .features import AudioFeature, BrightnessFeature, SizeFeature
+from .packets import AVPacket, FramePacket
+from .extractors import (
+    attach_audio_feature,
+    attach_brightness_feature,
+    attach_size_feature,
+)
+
+__all__ = [
+    "AudioFeature",
+    "BrightnessFeature",
+    "SizeFeature",
+    "FramePacket",
+    "AVPacket",
+    "attach_brightness_feature",
+    "attach_audio_feature",
+    "attach_size_feature",
 from .decoder import FFmpegDecoder
 from .packets import AVPacket, AudioPacket, FramePacket, iter_av_packets
 from .pipeline import Pipeline
